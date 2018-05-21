a63257852961856cd4dea481604e0ccb1763bb99
zaxbatch <34843086+zaxbatch@users.noreply.github.com>
2018-05-20 22:11:37 -0400

    list displays

--- src/app/component01/component01.component.ts
+++ src/app/component01/component01.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Game } from '../game'
+import { Game } from '../game';
+import { GAMES } from '../mock-games';
 
 @Component({
   selector: 'app-component01',
@@ -7,13 +8,18 @@ import { Game } from '../game'
   styleUrls: ['./component01.component.css']
 })
 export class Component01Component implements OnInit {
+  games = GAMES;
+
+  /*
   game: Game = {
     id: 1,
     title: 'Mario',
     rating: 4.5
   }
+*/
+  constructor() {
 
-  constructor() { }
+    }
 
   ngOnInit() {
   }
---
+++ src/app/mock-games.ts
@@ -0,0 +1,10 @@
+import { Game } from './game';
+
+export const GAMES: Game[] = [
+  { id: 11, title: 'Mario', rating: 4.5 },
+  { id: 12, title: 'Sonic', rating: 4.4 },
+  { id: 13, title: 'Smash Bros', rating: 3.9 },
+  { id: 14, title: 'GTA', rating: 4.0 },
+  { id: 15, title: 'Boogerman', rating: 4.2 },
+  { id: 16, title: 'Crazy Taxi', rating: 3.5 }
+];
